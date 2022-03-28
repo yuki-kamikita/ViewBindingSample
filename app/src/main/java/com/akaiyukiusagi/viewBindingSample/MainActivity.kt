a2a27dc4b2e1518ea1ccdc0076ebf8c20cc6b136
shuhei.kiyosawa <shuhei.kiyosawa@karadanote.jp>
2022-03-28 18:21:52 +0900

    init project

---
+++ app/src/main/java/com/akaiyukiusagi/viewBindingSample/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.akaiyukiusagi.viewBindingSample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.akaiyukiusagi.viewBindingSample.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+}
